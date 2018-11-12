cbb9372da1b905bb4d247784bd24b64aa184beda
Anderson Lourenco Gianotti <anderson.gianotti@gmail.com>
2018-11-12 00:25:15 -0200

    Refatoração da classe TesteFatorial.javae inclusão de comentarios em Fatorial.java

--- Fatorial.java
+++ Fatorial.java
@@ -1,14 +1,12 @@
 public class Fatorial {
-
-
-
-public int calcularFatorial(int n) {
-	int resultado = 1;
-        for(int i = 2 ;i<=n;i++)
-            resultado*=i;
+	// Versao iterativa do calculo do fatorial
+	public int calcularFatorial(int n) {
+		int resultado = 1;
+	        for(int i = 2 ;i<=n;i++)
+        	    resultado*=i;
 	
-	return resultado;
+		return resultado;
 
-}
+	}
 
 }
